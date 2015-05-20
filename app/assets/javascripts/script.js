21a1a4757e46e599092d45829b783f5db7953d8c
lyman wong <lymanwong@gmail.com>
2015-05-20 15:20:29 -0700

    got it to work

--- app/assets/javascripts/script.js
+++ app/assets/javascripts/script.js
@@ -1,10 +1,23 @@
-SC.initialize({
-  client_id:"lydog"
-});
-
-$(document).ready(function(){
-  SC.get('/tracks/293',function(track){
-    SC.oEmbed(track.permalink_url,
-      document.getElementById('player'));
+function playSomeSound(genre){
+  SC.get('/tracks', {
+    genres: genre,
+  }, function(tracks){
+    var random = Math.floor(Math.random()*49);
+  SC.oEmbed(tracks[random].uri, {auto_play:true}, document.getElementById('player'));
   });
-});
+}
+
+window.onload = function(){
+  SC.initialize({
+    client_id: 'ef10d229673c01a84693af525e10e933'
+  });
+
+  var menuLinks = document.getElementsByClassName('genre');
+  for(var i=0; i<menuLinks.length; i++) {
+    var menuLink = menuLinks[i];
+    menuLink.onclick = function(e) {
+      e.preventDefault();
+      playSomeSound(menuLink.innerHTML);
+    };
+  };
+};
